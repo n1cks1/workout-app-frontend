7ef247514dd7af4a8c955be9e9c19fbe51ba76ba
n1cks1 <nikitasib015@icloud.com>
2026-05-02 21:54:41 +0300

    refactor: remove Vite template files, update deps and config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const fromSrc = (p = '') => path.resolve(__dirname, './src', p)
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@styles': fromSrc('assets/styles'),
+			'@components': fromSrc('components'),
+			'@hooks': fromSrc('hooks'),
+			'@pages': fromSrc('pages'),
+			'@routes': fromSrc('routes')
+		}
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `
+          @use "@styles/_variables" as *;
+          @use "@styles/_animations" as *;
+        `
+			}
+		}
+	}
 })
+
+// CSS без импортов + пример настройки алиасов
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//       '@styles': path.resolve(__dirname, './src/styles'),
+//     },
+//   },
+//   css: {
+//     preprocessorOptions: {
+//       scss: {
+//         additionalData: `
+//           @use "@styles/abstracts/variables" as *;
+//           @use "@styles/abstracts/mixins" as *;
+//         `,
+//       },
+//     },
+//   },
+// })
