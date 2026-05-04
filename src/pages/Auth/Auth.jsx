599f9edbb7e690522919789c86ff81e7be8c8de5
n1cks1 <nikitasib015@icloud.com>
2026-05-04 18:45:33 +0300

    feat(auth): add auth page

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,7 +1,60 @@
 import Layout from '@components/layout/Layout'
+import Button from '@ui/button/Button'
+import Field from '@ui/field/Field'
+import Loader from '@ui/Loader'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import styles from './Auth.module.scss'
 
 const Auth = () => {
-	return <Layout>auth page</Layout>
+	const [type, setType] = useState('auth')
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm({
+		mode: 'onChange'
+	})
+
+	const onSubmit = data => {
+		//type
+		console.log(data)
+	}
+	return (
+		<>
+			<Layout
+				heading="Login"
+				bgImage="/images/auth-bg.png"
+			/>
+			<div className="wrapper-inner-page">
+				{true && <Loader />}
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<Field
+						type="email"
+						placeholder="Enter Email"
+						name="email"
+						register={register}
+						options={{ required: 'Email is required' }}
+						error={errors?.email?.message}
+					/>
+
+					<Field
+						type="password"
+						placeholder="Enter password"
+						name="password"
+						register={register}
+						options={{ required: 'Password is required' }}
+						error={errors?.password?.message}
+					/>
+					<div className={styles.wrapperButtons}>
+						<Button clickHandler={() => setType('auth')}>Log In</Button>
+						<Button clickHandler={() => setType('register')}>Sign Up</Button>
+					</div>
+				</form>
+			</div>
+		</>
+	)
 }
 
 export default Auth
---
+++ src/ui/Loader.jsx
@@ -0,0 +1,12 @@
+const Loader = () => {
+	return (
+		<img
+			src="/images/three-dots.svg"
+			alt="Loader"
+			width={'90px'}
+			draggable={false}
+		/>
+	)
+}
+
+export default Loader
